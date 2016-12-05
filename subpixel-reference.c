f267c93b5b085603980af82b75718e78fd91e8d7
Nicolas Hurtubise <316k@legtux.org>
2016-12-05 02:17:46 -0500

    Cleans code and adds infos about generated values

--- subpixel-reference.c
+++ subpixel-reference.c
@@ -20,18 +20,25 @@ float get(float** matrix, int i, int j, int w, int h) {
 
 int main(char argc, char** argv) {
 
-    int nthreads = 4, i, j, k, foo, shift, w = 1920, h = 1080;
+    int nthreads = 4, i, j, k, shift, w = 1920, h = 1080;
 
-    float decalage = 0.0;
+    float x = 0.0, y = 0.0;
 
     // Args parsing
     ARGBEGIN
     ARG_CASE('t')
         nthreads = ARGI;
 
-    ARG_CASE('d')
-        decalage = ARGF;
-        if(!(decalage <= 1 && decalage >= 0))
+    ARG_CASE('x')
+        x = 0.5 + ARGF / 2;
+    
+        if(!(x <= 1 && x >= 0))
+            goto usage;
+
+    ARG_CASE('y')
+        y = 0.5 + ARGF / 2;
+    
+        if(!(y <= 1 && y >= 0))
             goto usage;
 
     ARG_CASE('w')
@@ -42,21 +49,47 @@ int main(char argc, char** argv) {
     
     WRONG_ARG
         usage:
-        printf("usage: %s [-t nb_threads=%d] [-d decalage=%f]\n"
-               "\t[-w %d] [-h %d]\n",
-               argv0, nthreads, decalage, w, h);
+        printf("usage: %s [-t nb_threads=%d] [-x del-x=%f] [-y del-y=%f]\n",
+               "\t[-w %d] [-h %d]\n"
+               "\twith x,y element of -0.5..0.5\n",
+               argv0, nthreads, x, y, w, h);
     exit(1);
     
-    ARGEND;
+    ARGEND
     
     float*** out = malloc_f32cube(3, w, h);
     
     for(i=0; i < h; i++)
         for(j=0; j<w; j++) {
-            out[X][i][j] = j;
-            out[Y][i][j] = i + decalage;
+            out[X][i][j] = j + x;
+            out[Y][i][j] = i + y;
             out[DIST][i][j] = 0;
         }
+    
+    FILE* vals;
+    
+    for(k=0; k<2; k++) {
+        float min = 100000;
+        float dec = 0;
+        float max = -100000;
+
+        if(k == 0)
+            vals = fopen("subpixel-x-ref", "w");
+        else
+            vals = fopen("subpixel-y-ref", "w");
+        
+        for(i=1; i<h-1; i++)
+            for(j=1; j<w-1; j++) {
+                dec += out[k][i][j] - floor(out[k][i][j]);
+                min = fmin(min, out[k][i][j] - floor(out[k][i][j]));
+                max = fmax(max, out[k][i][j] - floor(out[k][i][j]));
+                fprintf(vals, "%f\n", out[k][i][j] - floor(out[k][i][j]));
+            }
+        
+        dec /= (h - 2) * (w - 2);
+        fprintf(stderr, "avg=%f min=%f max=%f\n", dec, min, max);
+        fclose(vals);
+    }
     
     save_color_map(NULL, out, w, h, w, h, 1);
     
