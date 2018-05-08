26ebd9f82d4862071297192b7bed143e82362980
Nicolas Hurtubise <316k@legtux.org>
2018-05-08 19:00:01 -0400

    Correct subpixel-translation code

--- translation.c
+++ translation.c
@@ -1,3 +1,6 @@
+/*
+  Apply sub-pixel translation to a PGM image
+ */
 #include <stdlib.h>
 #include <stdio.h>
 #include <omp.h>
@@ -18,9 +21,12 @@ float get(float** matrix, int i, int j, int w, int h) {
         j = 0;
     if(j > w - 1)
         j = w - 1;
-    
+
     return matrix[i][j];
-    
+}
+
+int sign(float x) {
+    return x >= 0 ? 1 : -1;
 }
 
 int main(char argc, char** argv) {
@@ -38,47 +44,42 @@ int main(char argc, char** argv) {
 
     ARG_CASE('x')
         x = ARGF;
-    
-        if(!(x <= 1 && x >= 0))
+
+        if(!(x <= 1 && x >= -1))
             goto usage;
 
     ARG_CASE('y')
         y = ARGF;
-    
-        if(!(y <= 1 && y >= 0))
+
+        if(!(y <= 1 && y >= -1))
             goto usage;
 
-    ARG_CASE('w')
-        w = ARGI;
-    
-    ARG_CASE('h')
-        h = ARGI;
-    
     WRONG_ARG
         usage:
         printf("usage: %s [-t nb_threads=%d] [-x del-x=%f]\n"
-               "\t[-y del-y=%f] filename\n"
-               "\twith x,y element of -0.5..0.5\n",
+               "\t[-y del-y=%f] image.pgm > out.pgm\n",
                argv0, nthreads, x, y);
         exit(1);
-    
+
     ARGEND
-    
+
     if(argc < 1) goto usage;
-    
+
     float** in = load_pgm(argv[argc - 1], &w, &h);
 
     float** out = malloc_f32matrix(w, h);
 
+    int decalage_x = -sign(x), decalage_y = -sign(y);
+
     for(i=0; i < h; i++)
         for(j=0; j < w; j++) {
-            out[i][j] = get(in, i, j, w, h) * (1 - x)*(1 - y)
-                + get(in, i, j + 1, w, h) * (1 - y)*x
-                + get(in, i + 1, j, w, h) * (1 - x)*y
-                + get(in, i + 1, j + 1, w, h) * x*y;
+            out[i][j] = get(in, i, j, w, h) * (1 - fabs(x))*(1 - fabs(y))
+                + get(in, i, j + decalage_x, w, h) * (1 - fabs(y))*fabs(x)
+                + get(in, i + decalage_y, j, w, h) * (1 - fabs(x))*fabs(y)
+                + get(in, i + decalage_y, j + decalage_x, w, h) * fabs(x*y);
         }
-            
+
     save_pgm(NULL, out, w, h, 8);
-    
+
     return 0;
 }
