064ee20b9ff5ae8327c4a3bcfa8407cbd0a7644c
Nicolas Hurtubise <316k@legtux.org>
2018-05-08 21:24:23 -0400

    Update subpixel reference code

--- subpixel-reference.c
+++ subpixel-reference.c
@@ -15,7 +15,7 @@ float get(float** matrix, int i, int j, int w, int h) {
     }
 
     return matrix[i][j];
-    
+
 }
 
 int main(char argc, char** argv) {
@@ -30,44 +30,43 @@ int main(char argc, char** argv) {
         nthreads = ARGI;
 
     ARG_CASE('x')
-        x = 0.5 + ARGF / 2;
-    
-        if(!(x <= 1 && x >= 0))
+        x = ARGF;
+
+        if(!(x <= 1 && x >= -1))
             goto usage;
 
     ARG_CASE('y')
-        y = 0.5 + ARGF / 2;
-    
-        if(!(y <= 1 && y >= 0))
+        y = ARGF;
+
+        if(!(y <= 1 && y >= -1))
             goto usage;
 
     ARG_CASE('w')
         w = ARGI;
-    
+
     ARG_CASE('h')
         h = ARGI;
-    
+
     WRONG_ARG
         usage:
         printf("usage: %s [-t nb_threads=%d] [-x del-x=%f] [-y del-y=%f]\n",
-               "\t[-w %d] [-h %d]\n"
-               "\twith x,y element of -0.5..0.5\n",
+               "\t[-w %d] [-h %d]\n",
                argv0, nthreads, x, y, w, h);
     exit(1);
-    
+
     ARGEND
-    
+
     float*** out = malloc_f32cube(3, w, h);
-    
+
     for(i=0; i < h; i++)
         for(j=0; j<w; j++) {
             out[X][i][j] = j + x;
             out[Y][i][j] = i + y;
             out[DIST][i][j] = 0;
         }
-    
+
     FILE* vals;
-    
+
     for(k=0; k<2; k++) {
         float min = 100000;
         float dec = 0;
@@ -77,7 +76,7 @@ int main(char argc, char** argv) {
             vals = fopen("subpixel-x-ref", "w");
         else
             vals = fopen("subpixel-y-ref", "w");
-        
+
         for(i=1; i<h-1; i++)
             for(j=1; j<w-1; j++) {
                 dec += out[k][i][j] - floor(out[k][i][j]);
@@ -85,13 +84,13 @@ int main(char argc, char** argv) {
                 max = fmax(max, out[k][i][j] - floor(out[k][i][j]));
                 fprintf(vals, "%f\n", out[k][i][j] - floor(out[k][i][j]));
             }
-        
+
         dec /= (h - 2) * (w - 2);
         fprintf(stderr, "avg=%f min=%f max=%f\n", dec, min, max);
         fclose(vals);
     }
-    
+
     save_color_map(NULL, out, w, h, w, h, 1);
-    
+
     return 0;
 }
