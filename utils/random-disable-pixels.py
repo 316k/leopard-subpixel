f0f8bbc3f77bb2f9537e36be34b0a8054e06af3e
Nicolas Hurtubise <316k@legtux.org>
2019-12-18 16:36:08 -0500

    Add --white in utils/random-disable-pixels.py

--- utils/random-disable-pixels.py
+++ utils/random-disable-pixels.py
@@ -10,6 +10,7 @@ arg_parser.add_argument("seed", help="Random mask seed", type=int, default=1)
 arg_parser.add_argument("img", help="Input image")
 arg_parser.add_argument("out", help="Maksed image")
 arg_parser.add_argument("--block-size", help="Size of a cut out block", type=int, default=5)
+arg_parser.add_argument("--white", help="Mask with white instead of black", action='store_true')
 
 args = arg_parser.parse_args()
 
@@ -21,17 +22,23 @@ np.random.seed(args.seed)
 
 offsets = np.random.randint(0, bs, (2,))
 
-h, w = img.shape
-
-print(w, h)
+h, w = img.shape[:2]
 
 low_res_mask = np.random.randint(0, 2, (h // bs + 1, w // bs + 1))
 
-full_mask = np.zeros((h, w))
+full_mask = np.zeros(img.shape)
 
 for y in range(h):
     for x in range(w):
-        full_mask[y][x] = low_res_mask[(y - offsets[0]) // bs, (x - offsets[1]) // bs]
+        if len(img.shape) == 2:
+            full_mask[y, x] = low_res_mask[(y - offsets[0]) // bs, (x - offsets[1]) // bs]
+        else:
+            full_mask[y, x, :] = low_res_mask[(y - offsets[0]) // bs, (x - offsets[1]) // bs]
 
-write_img(args.out, img * full_mask, depth=16 if depth == 65535 else 8)
+out_img = img * full_mask
+
+if args.white:
+    out_img[full_mask == 0] = depth
+
+write_img(args.out, out_img, depth=16 if depth == 65535 else 8)
 
